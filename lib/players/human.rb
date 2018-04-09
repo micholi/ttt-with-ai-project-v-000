dac23abed657fc148ca93c0284b4498041f1e216
Michelle Olivieri <michelleo72@gmail.com>
2018-04-09 19:59:23 +0000

    automatically backed up by learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -2,14 +2,8 @@ module Players
   class Human < Player
 
     def move(board)
-      if board.turn_count == 0
-        display.board
-        puts "Please enter 1-9:"
-        gets.strip
-      else
       puts "Please enter 1-9:"
       gets.strip
-    end
     end
 
   end
