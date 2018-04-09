5b3e73c0a1cd676cdfaa1c6831c9db8813425080
Michelle Olivieri <michelleo72@gmail.com>
2018-04-09 21:43:32 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -8,10 +8,10 @@ module Players
       move = ""
         if !board.taken?(5)
           move = "5"
-        elsif board.turn_count == 1
-          move = "1" || move = "3" || move = "7" || move = "9"
-        elsif board.turn_count == 2
-          move = "1" || move = "3" || move = "7" || move = "9"
+      #  elsif board.turn_count == 1
+      #    move = "1" || move = "3" || move = "7" || move = "9"
+      #  elsif board.turn_count == 2
+      #    move = "1" || move = "3" || move = "7" || move = "9"
         end
     end
 
